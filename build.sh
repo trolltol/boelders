5e755aa896e65c57bf24cd2b1336e2e700443eee
trolltol <trolltol21@gmail.com>
2023-03-18 21:16:52 +0700

    more [skip-ci]

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone local_manifest --depth 1 -b tissot-bananadroidCoreGApps .repo/local_manifests
+git clone local_manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build romsl
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
+
 source build/envsetup.sh
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=
@@ -18,6 +19,7 @@ tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
 mka bacon -j8  > reading
+
 retVal=$?
 timeEnd
 statusBuild
