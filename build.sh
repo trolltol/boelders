473c8f1106ebab25f89bfb2ff9b2ee9d759208df
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-03-21 19:44:15 +0700

    AFAKAH SUDAH BISA

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_m
 git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-bananadroid-vanilla .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 source build/envsetup.sh
